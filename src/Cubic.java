c462012ba7676a16eb69f2142302b42614f11ec4
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-07 18:41:39 +1300

    Add conditions for integers

--- src/Cubic.java
+++ src/Cubic.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 
 public class Cubic {
     public static void main(String[] args) {
+        String word;
+        int push;
+        char direction;
+
         Scanner sc = new Scanner(System.in);
         System.out.println("단어?");
         System.out.print(">>");
-        String word = sc.nextLine();
+        word = sc.nextLine();
         System.out.println("정수?");
         System.out.print(">>");
-        int push = sc.nextInt();
+        push = sc.nextInt();
+        while (push < -100 || push >= 100) {
+                push = sc.nextInt();
+            }
         System.out.println("L or R?");
-        char direction = sc.next().charAt(0);
+        direction = sc.next().charAt(0);
 
 
-
+        }
     }
-}
+
