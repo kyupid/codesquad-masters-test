6faabef0ea2b48d44999af12f74d8097344955cd
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-07 18:31:46 +1300

    Add scanner

---
+++ src/Cubic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Cubic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("단어?");
+        System.out.print(">>");
+        String word = sc.nextLine();
+        System.out.println("정수?");
+        System.out.print(">>");
+        int push = sc.nextInt();
+        System.out.println("L or R?");
+        char direction = sc.next().charAt(0);
+
+
+
+    }
+}
