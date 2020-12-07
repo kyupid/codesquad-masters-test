707675574c97bcd2d71bbb0e4e0077d023d7309b
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-08 09:48:25 +1300

    Declare new array that gets strings using split

--- src/Cubic.java
+++ src/Cubic.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 public class Cubic {
 
     public static void main(String[] args) {
     String[][] cube = {{"R", "R", "W"},
                         {"G", "C", "W"},
                         {"G", "B", "B"}};
+
+    Scanner sc = new Scanner(System.in);
+    String[] str = sc.nextLine().split(" ");
+
+
 
     }
 }
