248cac6ed317a11c78e6dd0334e85a0f884e8465
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-08 09:27:19 +1300

    Declare a double array

--- src/Cubic.java
+++ src/Cubic.java
@@ -1,6 +1,9 @@
 public class Cubic {
 
     public static void main(String[] args) {
+    String[][] cube = {{"R", "R", "W"},
+                        {"G", "C", "W"},
+                        {"G", "B", "B"}};
 
     }
 }
