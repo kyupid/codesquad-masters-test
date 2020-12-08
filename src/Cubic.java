5586b398dcb16f6aedd516fc24d33f9a8d058ee8
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-08 15:00:02 +1300

    Modify values when resetting array It was just slips

--- src/Cubic.java
+++ src/Cubic.java
@@ -6,13 +6,17 @@ public class Cubic {
             {"G", "B", "B"}};
 
     private Scanner sc = new Scanner(System.in);
-    private String[] str = sc.nextLine().split(" ");
+    private String[] str;
 
     private String s;
 
 
     Cubic() {
-        mix();
+        while (true) {
+            System.out.print("CUBE >> ");
+            str = sc.nextLine().split(" ");
+            mix();
+        }
     }
 
     private void mix() {
@@ -27,62 +31,60 @@ public class Cubic {
 
             if (str[i].equals("U'")) {
                 s = cube[0][0] + cube[0][1] + cube[0][2];
-                s = s.substring(2) + s.substring(0, 2); // 결과물
-                cube[0][0] = s.substring(0, 1);
-                cube[0][1] = s.substring(1, 2);
-                cube[0][2] = s.substring(2, 3);
-
-            }
-
-            if (str[i].equals("R")) {
-                s = cube[0][2] + cube[1][1] + cube[2][2];
-                s = s.substring(1) + s.substring(0, 1);
-                cube[0][0] = s.substring(0, 1);
-                cube[0][1] = s.substring(1, 2);
-                cube[0][2] = s.substring(2, 3);
-            }
-
-            if (str[i].equals("R'")) {
-                s = cube[0][2] + cube[1][1] + cube[2][2];
                 s = s.substring(2) + s.substring(0, 2);
                 cube[0][0] = s.substring(0, 1);
                 cube[0][1] = s.substring(1, 2);
                 cube[0][2] = s.substring(2, 3);
+            }
+
+
+            if (str[i].equals("R")) {
+                s = cube[0][2] + cube[1][2] + cube[2][2];
+                s = s.substring(1) + s.substring(0, 1);
+                cube[0][2] = s.substring(0, 1);
+                cube[1][2] = s.substring(1, 2);
+                cube[2][2] = s.substring(2, 3);
+            }
+
+            if (str[i].equals("R'")) {
+                s = cube[0][2] + cube[1][2] + cube[2][2];
+                s = s.substring(2) + s.substring(0, 2);
+                cube[0][2] = s.substring(0, 1);
+                cube[1][2] = s.substring(1, 2);
+                cube[2][2] = s.substring(2, 3);
             }
 
             if (str[i].equals("L")) {
                 s = cube[0][0] + cube[1][0] + cube[2][0];
                 s = s.substring(2) + s.substring(0, 2);
                 cube[0][0] = s.substring(0, 1);
-                cube[0][1] = s.substring(1, 2);
-                cube[0][2] = s.substring(2, 3);
+                cube[1][0] = s.substring(1, 2);
+                cube[2][0] = s.substring(2, 3);
             }
 
             if (str[i].equals("L'")) {
                 s = cube[0][0] + cube[1][0] + cube[2][0];
                 s = s.substring(1) + s.substring(0, 1);
                 cube[0][0] = s.substring(0, 1);
-                cube[0][1] = s.substring(1, 2);
-                cube[0][2] = s.substring(2, 3);
+                cube[1][0] = s.substring(1, 2);
+                cube[2][0] = s.substring(2, 3);
             }
 
             if (str[i].equals("B")) {
                 s = cube[2][0] + cube[2][1] + cube[2][2];
                 s = s.substring(2) + s.substring(0, 2);
-                cube[0][0] = s.substring(0, 1);
-                cube[0][1] = s.substring(1, 2);
-                cube[0][2] = s.substring(2, 3);
+                cube[2][0] = s.substring(0, 1);
+                cube[2][1] = s.substring(1, 2);
+                cube[2][2] = s.substring(2, 3);
             }
 
             if (str[i].equals("B'")) {
                 s = cube[2][0] + cube[2][1] + cube[2][2];
                 s = s.substring(1) + s.substring(0, 1);
-                cube[0][0] = s.substring(0, 1);
-                cube[0][1] = s.substring(1, 2);
-                cube[0][2] = s.substring(2, 3);
+                cube[2][0] = s.substring(0, 1);
+                cube[2][1] = s.substring(1, 2);
+                cube[2][2] = s.substring(2, 3);
             }
-
-
             System.out.println(str[i]);
             printArray();
         }
@@ -100,7 +102,6 @@ public class Cubic {
     }
 
     public static void main(String[] args) {
-        System.out.print("CUBE >> ");
         new Cubic();
     }
 }
