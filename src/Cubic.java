3e7941e76283205d493f047e2d5428804f5a5fee
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-10 09:36:49 +1300

    Implement printing the cube

--- src/Cubic.java
+++ src/Cubic.java
@@ -1,7 +1,8 @@
 public class Cubic {
 
     public static void main(String[] args) {
-
+       Game g = new Game();
+       g.run();
     }
 }
 
---
+++ src/Game.java
@@ -0,0 +1,51 @@
+class Game {
+
+    String[][] up = {{"W", "W", "W"}, {"W", "W", "W"}, {"W", "W", "W"}};
+    String[][] left = {{"O", "O", "O"}, {"O", "O", "O"}, {"O", "O", "O"}};
+    String[][] front = {{"G", "G", "G"}, {"G", "G", "G"}, {"G", "G", "G"}};
+    String[][] right = {{"R", "R", "R"}, {"R", "R", "R"}, {"R", "R", "R"}};
+    String[][] back = {{"B", "B", "B"}, {"B", "B", "B"}, {"B", "B", "B"}};
+    String[][] down = {{"Y", "Y", "Y"}, {"Y", "Y", "Y"}, {"Y", "Y", "Y"}};
+
+    void run() {
+        printAllLine();
+
+    }
+
+///////////////////////////Print all part of the cube///////////////////////////////
+    private void printAllLine() {
+        printUpDown(up);
+        printMiddleOneLine(0);
+        printMiddleOneLine(1);
+        printMiddleOneLine(2);
+        printUpDown(down);
+    }
+
+    private void printMiddlePartLine(int num, String[][] test) {
+        for (int i = 0; i < 3; i++) {
+            System.out.print(test[num][i] + " ");
+        }
+        System.out.print("\t\t");
+    }
+
+
+    private void printMiddleOneLine(int i) {
+        printMiddlePartLine(i, left);
+        printMiddlePartLine(i, front);
+        printMiddlePartLine(i, right);
+        printMiddlePartLine(i, back);
+        System.out.println();
+    }
+
+    private void printUpDown(String[][] upDown) {
+        System.out.println();
+        for (String[] e : upDown) {
+            System.out.print("\t\t\t");
+            for (String ee : e) {
+                System.out.print(ee + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
