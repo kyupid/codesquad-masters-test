48186162e37847240fd6877c2a18431d9a97552e
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-11 09:55:34 +1300

    Add turning cube of all cases

--- src/Game.java
+++ src/Game.java
@@ -30,8 +30,382 @@ class Game {
             if (e.equals("F'")) {
                 FR();
             }
+            if (e.equals("R")) {
+                R();
+            }
+            if (e.equals("R'")) {
+                RR();
+            }
+            if (e.equals("U")) {
+                U();
+            }
+            if (e.equals("U'")) {
+                UR();
+            }
+            if (e.equals("B")) {
+                B();
+            }
+            if (e.equals("B'")) {
+                BR();
+            }
+            if (e.equals("L")) {
+                L();
+            }
+            if (e.equals("L'")) {
+                LR();
+            }
+            if (e.equals("D")) {
+                D();
+            }
+            if (e.equals("D'")) {
+                DR();
+            }
         }
         printAllLine();
+    }
+    private void DR() {
+        array[0] = arr_left[2][0];
+        array[1] = arr_left[2][1];
+        array[2] = arr_left[2][2];
+
+        array[3] = arr_front[2][0];
+        array[4] = arr_front[2][1];
+        array[5] = arr_front[2][2];
+
+        array[6] = arr_right[2][0];
+        array[7] = arr_right[2][1];
+        array[8] = arr_right[2][2];
+
+        array[9] = arr_back[2][0];
+        array[10] = arr_back[2][1];
+        array[11] = arr_back[2][2];
+
+        arr_left[2][0] = array[3];
+        arr_left[2][1] = array[4];
+        arr_left[2][2] = array[5];
+
+        arr_front[2][0] = array[6];
+        arr_front[2][1] = array[7];
+        arr_front[2][2] = array[8];
+
+        arr_right[2][0] = array[9];
+        arr_right[2][1] = array[10];
+        arr_right[2][2] = array[11];
+
+        arr_back[2][0] = array[0];
+        arr_back[2][1] = array[1];
+        arr_back[2][2] = array[2];
+    }
+
+    private void D() {
+        array[0] = arr_left[2][0];
+        array[1] = arr_left[2][1];
+        array[2] = arr_left[2][2];
+
+        array[3] = arr_front[2][0];
+        array[4] = arr_front[2][1];
+        array[5] = arr_front[2][2];
+
+        array[6] = arr_right[2][0];
+        array[7] = arr_right[2][1];
+        array[8] = arr_right[2][2];
+
+        array[9] = arr_back[2][0];
+        array[10] = arr_back[2][1];
+        array[11] = arr_back[2][2];
+
+        arr_left[2][0] = array[9];
+        arr_left[2][1] = array[10];
+        arr_left[2][2] = array[11];
+
+        arr_front[2][0] = array[0];
+        arr_front[2][1] = array[1];
+        arr_front[2][2] = array[2];
+
+        arr_right[2][0] = array[3];
+        arr_right[2][1] = array[4];
+        arr_right[2][2] = array[5];
+
+        arr_back[2][0] = array[6];
+        arr_back[2][1] = array[7];
+        arr_back[2][2] = array[8];
+    }
+
+    private void LR() {
+        array[0] = arr_up[0][0];
+        array[1] = arr_up[1][0];
+        array[2] = arr_up[2][0];
+
+        array[3] = arr_front[0][0];
+        array[4] = arr_front[1][0];
+        array[5] = arr_front[2][0];
+
+        array[6] = arr_back[0][2];
+        array[7] = arr_back[1][2];
+        array[8] = arr_back[2][2];
+
+        array[9] = arr_down[0][0];
+        array[10] = arr_down[1][0];
+        array[11] = arr_down[2][0];
+
+        arr_up[0][0] = array[3];
+        arr_up[1][0] = array[4];
+        arr_up[2][0] = array[5];
+
+        arr_front[0][0] = array[9];
+        arr_front[1][0] = array[10];
+        arr_front[2][0] = array[11];
+
+        arr_back[0][2] = array[2];
+        arr_back[1][2] = array[1];
+        arr_back[2][2] = array[0];
+
+        arr_down[0][0] = array[8];
+        arr_down[1][0] = array[7];
+        arr_down[2][0] = array[6];
+
+    }
+
+    private void L() {
+        array[0] = arr_up[0][0];
+        array[1] = arr_up[1][0];
+        array[2] = arr_up[2][0];
+
+        array[3] = arr_front[0][0];
+        array[4] = arr_front[1][0];
+        array[5] = arr_front[2][0];
+
+        array[6] = arr_back[0][2];
+        array[7] = arr_back[1][2];
+        array[8] = arr_back[2][2];
+
+        array[9] = arr_down[0][0];
+        array[10] = arr_down[1][0];
+        array[11] = arr_down[2][0];
+
+        arr_up[0][0] = array[8];
+        arr_up[1][0] = array[7];
+        arr_up[2][0] = array[6];
+
+        arr_front[0][0] = array[0];
+        arr_front[1][0] = array[1];
+        arr_front[2][0] = array[2];
+
+        arr_back[0][2] = array[11];
+        arr_back[1][2] = array[10];
+        arr_back[2][2] = array[9];
+
+        arr_down[0][0] = array[3];
+        arr_down[1][0] = array[4];
+        arr_down[2][0] = array[5];
+
+    }
+
+    private void BR() {
+        array[0] = arr_up[0][0];
+        array[1] = arr_up[0][1];
+        array[2] = arr_up[0][2];
+
+        array[3] = arr_left[0][0];
+        array[4] = arr_left[1][0];
+        array[5] = arr_left[2][0];
+
+        array[6] = arr_right[0][2];
+        array[7] = arr_right[1][2];
+        array[8] = arr_right[2][2];
+
+        array[9] = arr_down[2][0];
+        array[10] = arr_down[2][1];
+        array[11] = arr_down[2][2];
+
+        arr_up[0][0] = array[5];
+        arr_up[0][1] = array[4];
+        arr_up[0][2] = array[3];
+
+        arr_left[0][0] = array[9];
+        arr_left[1][0] = array[10];
+        arr_left[2][0] = array[11];
+
+        arr_right[0][2] = array[0];
+        arr_right[1][2] = array[1];
+        arr_right[2][2] = array[2];
+
+        arr_down[2][0] = array[8];
+        arr_down[2][1] = array[7];
+        arr_down[2][2] = array[6];
+    }
+
+    private void B() {
+        array[0] = arr_up[0][0];
+        array[1] = arr_up[0][1];
+        array[2] = arr_up[0][2];
+
+        array[3] = arr_left[0][0];
+        array[4] = arr_left[1][0];
+        array[5] = arr_left[2][0];
+
+        array[6] = arr_right[0][2];
+        array[7] = arr_right[1][2];
+        array[8] = arr_right[2][2];
+
+        array[9] = arr_down[2][0];
+        array[10] = arr_down[2][1];
+        array[11] = arr_down[2][2];
+
+        arr_up[0][0] = array[6];
+        arr_up[0][1] = array[7];
+        arr_up[0][2] = array[8];
+
+        arr_left[0][0] = array[2];
+        arr_left[1][0] = array[1];
+        arr_left[2][0] = array[0];
+
+        arr_right[0][2] = array[11];
+        arr_right[1][2] = array[10];
+        arr_right[2][2] = array[9];
+
+        arr_down[2][0] = array[3];
+        arr_down[2][1] = array[4];
+        arr_down[2][2] = array[5];
+    }
+
+    private void UR() {
+        array[0] = arr_left[0][0];
+        array[1] = arr_left[0][1];
+        array[2] = arr_left[0][2];
+
+        array[3] = arr_front[0][0];
+        array[4] = arr_front[0][1];
+        array[5] = arr_front[0][2];
+
+        array[6] = arr_right[0][0];
+        array[7] = arr_right[0][1];
+        array[8] = arr_right[0][2];
+
+        array[9] = arr_back[0][0];
+        array[10] = arr_back[0][1];
+        array[11] = arr_back[0][2];
+
+        arr_left[0][0] = array[9];
+        arr_left[0][1] = array[10];
+        arr_left[0][2] = array[11];
+
+        arr_front[0][0] = array[0];
+        arr_front[0][1] = array[1];
+        arr_front[0][2] = array[2];
+
+        arr_right[0][0] = array[3];
+        arr_right[0][1] = array[4];
+        arr_right[0][2] = array[5];
+
+        arr_back[0][0] = array[6];
+        arr_back[0][1] = array[7];
+        arr_back[0][2] = array[8];
+
+    }
+
+    private void U() {
+        array[0] = arr_left[0][0];
+        array[1] = arr_left[0][1];
+        array[2] = arr_left[0][2];
+
+        array[3] = arr_front[0][0];
+        array[4] = arr_front[0][1];
+        array[5] = arr_front[0][2];
+
+        array[6] = arr_right[0][0];
+        array[7] = arr_right[0][1];
+        array[8] = arr_right[0][2];
+
+        array[9] = arr_back[0][0];
+        array[10] = arr_back[0][1];
+        array[11] = arr_back[0][2];
+
+        arr_left[0][0] = array[3];
+        arr_left[0][1] = array[4];
+        arr_left[0][2] = array[5];
+
+        arr_front[0][0] = array[6];
+        arr_front[0][1] = array[7];
+        arr_front[0][2] = array[8];
+
+        arr_right[0][0] = array[9];
+        arr_right[0][1] = array[10];
+        arr_right[0][2] = array[11];
+
+        arr_back[0][0] = array[0];
+        arr_back[0][1] = array[1];
+        arr_back[0][2] = array[2];
+
+    }
+
+    private void RR() {
+        array[0] = arr_up[0][2];
+        array[1] = arr_up[1][2];
+        array[2] = arr_up[2][2];
+
+        array[3] = arr_front[0][2];
+        array[4] = arr_front[1][2];
+        array[5] = arr_front[2][2];
+
+        array[6] = arr_back[0][0];
+        array[7] = arr_back[1][0];
+        array[8] = arr_back[2][0];
+
+        array[9] = arr_down[0][2];
+        array[10] = arr_down[1][2];
+        array[11] = arr_down[2][2];
+
+        arr_up[0][2] = array[8];
+        arr_up[1][2] = array[7];
+        arr_up[2][2] = array[6];
+
+        arr_front[0][2] = array[0];
+        arr_front[1][2] = array[1];
+        arr_front[2][2] = array[2];
+
+        arr_down[0][2] = array[3];
+        arr_down[1][2] = array[4];
+        arr_down[2][2] = array[5];
+
+        arr_back[0][0] = array[11];
+        arr_back[1][0] = array[10];
+        arr_back[2][0] = array[9];
+    }
+
+    private void R() {
+        array[0] = arr_up[0][2];
+        array[1] = arr_up[1][2];
+        array[2] = arr_up[2][2];
+
+        array[3] = arr_front[0][2];
+        array[4] = arr_front[1][2];
+        array[5] = arr_front[2][2];
+
+        array[6] = arr_back[0][0];
+        array[7] = arr_back[1][0];
+        array[8] = arr_back[2][0];
+
+        array[9] = arr_down[0][2];
+        array[10] = arr_down[1][2];
+        array[11] = arr_down[2][2];
+
+        arr_up[0][2] = array[3];
+        arr_up[1][2] = array[4];
+        arr_up[2][2] = array[5];
+
+        arr_front[0][2] = array[9];
+        arr_front[1][2] = array[10];
+        arr_front[2][2] = array[11];
+
+        arr_down[0][2] = array[8];
+        arr_down[1][2] = array[7];
+        arr_down[2][2] = array[6];
+
+        arr_back[0][0] = array[2];
+        arr_back[1][0] = array[1];
+        arr_back[2][0] = array[0];
+
     }
 
     private void FR() {
@@ -77,19 +451,22 @@ class Game {
         array[10] = arr_down[0][1];
         array[11] = arr_down[0][2];
 
-        arr_right[0][0] = array[0];
-        arr_right[1][0] = array[1];
-        arr_right[2][0] = array[2];
-        arr_up[2][2] = array[3];
-        arr_up[2][1] = array[4];
         arr_up[2][0] = array[5];
-        arr_down[0][2] = array[6];
-        arr_down[0][1] = array[7];
-        arr_down[0][0] = array[8];
+        arr_up[2][1] = array[4];
+        arr_up[2][2] = array[3];
         arr_left[0][2] = array[9];
         arr_left[1][2] = array[10];
         arr_left[2][2] = array[11];
+        arr_right[0][0] = array[0];
+        arr_right[1][0] = array[1];
+        arr_right[2][0] = array[2];
+        arr_down[0][0] = array[8];
+        arr_down[0][1] = array[7];
+        arr_down[0][2] = array[6];
+
     }
+
+
 
     ///////////////////////////Print all part of the cube///////////////////////////////
     private void printAllLine() {
