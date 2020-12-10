0222c4733c61be7577b6f3aa769cf1e510710e3a
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-10 14:19:18 +1300

    Add F of turnCube

--- src/Game.java
+++ src/Game.java
@@ -9,21 +9,54 @@ class Game {
     String[][] arr_back = {{"B", "B", "B"}, {"B", "B", "B"}, {"B", "B", "B"}};
     String[][] arr_down = {{"Y", "Y", "Y"}, {"Y", "Y", "Y"}, {"Y", "Y", "Y"}};
 
-    String up, left, front, right, back, down;
+    String[] array = new String[12];
 
     Scanner sc = new Scanner(System.in);
 
     void run() {
         printAllLine();
-        turnCube();
+        while(true) {
+            turnCube();
+        }
     }
 
     private void turnCube() {
+        System.out.print("CUBE >> ");
         String[] str = sc.nextLine().split(" ");
+        for (String e : str) {
+            if (e.equals("F")) {
 
+                array[0] = arr_up[2][0];
+                array[1] = arr_up[2][1];
+                array[2] = arr_up[2][2];
+                array[3] = arr_left[0][2];
+                array[4] = arr_left[1][2];
+                array[5] = arr_left[2][2];
+                array[6] = arr_right[0][0];
+                array[7] = arr_right[1][0];
+                array[8] = arr_right[2][0];
+                array[9] = arr_down[0][0];
+                array[10] = arr_down[0][1];
+                array[11] = arr_down[0][2];
+
+                arr_right[0][0] = array[0];
+                arr_right[1][0] = array[1];
+                arr_right[2][0] = array[2];
+                arr_up[2][2] = array[3];
+                arr_up[2][1] = array[4];
+                arr_up[2][0] = array[5];
+                arr_down[0][2] = array[6];
+                arr_down[0][1] = array[7];
+                arr_down[0][0] = array[8];
+                arr_left[0][2] = array[9];
+                arr_left[1][2] = array[10];
+                arr_left[2][2] = array[11];
+            }
+        }
+        printAllLine();
     }
 
-///////////////////////////Print all part of the cube///////////////////////////////
+    ///////////////////////////Print all part of the cube///////////////////////////////
     private void printAllLine() {
         printUpDown(arr_up);
         printMiddleOneLine(0);
