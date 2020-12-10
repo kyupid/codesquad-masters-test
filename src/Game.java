622c36c94caebdf331c7304f8856569de7bf0090
kyu-kim-kr <59721293+kyu-kim-kr@users.noreply.github.com>
2020-12-10 14:48:36 +1300

    Add F' of turnCube

--- src/Game.java
+++ src/Game.java
@@ -15,7 +15,7 @@ class Game {
 
     void run() {
         printAllLine();
-        while(true) {
+        while (true) {
             turnCube();
         }
     }
@@ -25,35 +25,70 @@ class Game {
         String[] str = sc.nextLine().split(" ");
         for (String e : str) {
             if (e.equals("F")) {
-
-                array[0] = arr_up[2][0];
-                array[1] = arr_up[2][1];
-                array[2] = arr_up[2][2];
-                array[3] = arr_left[0][2];
-                array[4] = arr_left[1][2];
-                array[5] = arr_left[2][2];
-                array[6] = arr_right[0][0];
-                array[7] = arr_right[1][0];
-                array[8] = arr_right[2][0];
-                array[9] = arr_down[0][0];
-                array[10] = arr_down[0][1];
-                array[11] = arr_down[0][2];
-
-                arr_right[0][0] = array[0];
-                arr_right[1][0] = array[1];
-                arr_right[2][0] = array[2];
-                arr_up[2][2] = array[3];
-                arr_up[2][1] = array[4];
-                arr_up[2][0] = array[5];
-                arr_down[0][2] = array[6];
-                arr_down[0][1] = array[7];
-                arr_down[0][0] = array[8];
-                arr_left[0][2] = array[9];
-                arr_left[1][2] = array[10];
-                arr_left[2][2] = array[11];
+                F();
+            }
+            if (e.equals("F'")) {
+                FR();
             }
         }
         printAllLine();
+    }
+
+    private void FR() {
+        array[0] = arr_up[2][0];
+        array[1] = arr_up[2][1];
+        array[2] = arr_up[2][2];
+        array[3] = arr_left[0][2];
+        array[4] = arr_left[1][2];
+        array[5] = arr_left[2][2];
+        array[6] = arr_right[0][0];
+        array[7] = arr_right[1][0];
+        array[8] = arr_right[2][0];
+        array[9] = arr_down[0][0];
+        array[10] = arr_down[0][1];
+        array[11] = arr_down[0][2];
+
+        arr_up[2][0] = array[6];
+        arr_up[2][1] = array[7];
+        arr_up[2][2] = array[8];
+        arr_left[0][2] = array[0];
+        arr_left[1][2] = array[1];
+        arr_left[2][2] = array[2];
+        arr_right[0][0] = array[11];
+        arr_right[1][0] = array[10];
+        arr_right[2][0] = array[9];
+        arr_down[0][0] = array[3];
+        arr_down[0][1] = array[4];
+        arr_down[0][2] = array[5];
+
+    }
+
+    private void F() {
+        array[0] = arr_up[2][0];
+        array[1] = arr_up[2][1];
+        array[2] = arr_up[2][2];
+        array[3] = arr_left[0][2];
+        array[4] = arr_left[1][2];
+        array[5] = arr_left[2][2];
+        array[6] = arr_right[0][0];
+        array[7] = arr_right[1][0];
+        array[8] = arr_right[2][0];
+        array[9] = arr_down[0][0];
+        array[10] = arr_down[0][1];
+        array[11] = arr_down[0][2];
+
+        arr_right[0][0] = array[0];
+        arr_right[1][0] = array[1];
+        arr_right[2][0] = array[2];
+        arr_up[2][2] = array[3];
+        arr_up[2][1] = array[4];
+        arr_up[2][0] = array[5];
+        arr_down[0][2] = array[6];
+        arr_down[0][1] = array[7];
+        arr_down[0][0] = array[8];
+        arr_left[0][2] = array[9];
+        arr_left[1][2] = array[10];
+        arr_left[2][2] = array[11];
     }
 
     ///////////////////////////Print all part of the cube///////////////////////////////
